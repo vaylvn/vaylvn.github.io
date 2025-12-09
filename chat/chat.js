d54aa26852b10de38c15d64b3efc9dcf1c793a84
vaylvn <164453259+vaylvn@users.noreply.github.com>
2025-12-09 19:32:56 +0000

    ui

--- chat/chat.js
+++ chat/chat.js
@@ -67,7 +67,7 @@ function addChatMessage(tags, message) {
 function formatTime(date) {
   const h = date.getHours().toString().padStart(2, "0");
   const m = date.getMinutes().toString().padStart(2, "0");
-  return `${h}:${m}`;
+  return ``;
 }
 
 function autoScroll() {
